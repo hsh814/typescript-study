1ee5ca27426d78ff35bb339cf04f095ff79dd010
hsh814 <hsh814@naver.com>
2020-08-03 14:30:43 +0900

    fileAPI: index.ts

---
+++ practice/src/fileApi/index.ts
@@ -0,0 +1,9 @@
+import {fileExists} from './fileExists';
+import {mkdir} from './mkdir-p';
+import {rmdir} from './rm-r';
+import {writeFile} from './writeFile';
+import {readFile} from './readFile';
+import {appendFile} from './appendFile';
+import {removeFile} from './removeFile';
+
+export {fileExists, mkdir, rmdir, writeFile, readFile, appendFile, removeFile};
